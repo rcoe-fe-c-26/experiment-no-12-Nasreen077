10de1fc32e21c94156f3728f4ba922d22cebeb97
Nasreen077 <nasreenansari720@gmail.com>
2026-02-12 22:40:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder:Nasreen
+# Date:12/2/26
 
 print("--- Extracting Words from Text File ---\n")
 
+
+
+with open("story.txt","r") as file:
+  content=file.read().split()
+var = int(input("Enter Length of Words: "))
+l = []
+
+for i in content:
+    i = i.lower().strip(".,!?")
+    if len(i) == var:
+        if i not in l:
+            l.append(i)
+
+
+print("words with length", var, "are:", l)
